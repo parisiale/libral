6f5ec6404a1ee17dce884a1ad3653cb8942f5502
John McCabe <john@johnmccabe.net>
2017-07-05 10:32:38 +0100

    (DI-92) Implement get methods in wrapper layer
    - get_types
    - get_resource
    - get_resources

--- lib/src/cwrapper.cc
+++ lib/src/cwrapper.cc
@@ -1,19 +1,19 @@
-#include <libral/cwrapper.hpp>
-
-#include <libral/ral.hpp>
-#include <libral/emitter/json_emitter.hpp>
-
+#include <iostream>
 #include <string>
 #include <vector>
+
+#include <libral/cwrapper.hpp>
+#include <libral/ral.hpp>
+#include <libral/emitter/json_emitter.hpp>
 
 namespace lib = libral;
 
 /* TODO in GOLand:
  *
- * getTypes()(string, error) --> string == MUST serialize vector as JSON array
- * getAllResources(type) (string, error) --> string == JSON object serialized
- * getResource(type, resource) (string, error) --> string == JSON object serialized
- * getSchema(type)(string, error) --> how to derive a JSON schema??? Use JsonSchema???
+ * [x] getTypes()(string, error) --> string == MUST serialize vector as JSON array
+ * [x] getAllResources(type) (string, error) --> string == JSON object serialized
+ * [x] getResource(type, resource) (string, error) --> string == JSON object serialized
+ * [ ] getSchema(type)(string, error) --> how to derive a JSON schema??? Use JsonSchema???
  *
  */
 
@@ -31,43 +31,45 @@ static char* str_to_cstr(const std::string& s) {
 // Public interface
 //
 
-// TODO(ale): decide between pointers & structs for calling / returning
-//            conventions and use a single approach
+uint8_t get_types(char **result) {
+    std::vector<std::string> data_dirs;
+    auto ral = lib::ral::create(data_dirs);
+    auto raw_types = ral->types();
+    lib::json_emitter em {};
+    auto types = em.parse_types(raw_types);
 
-char* get_all(char* type_name)
-{
+    *result = str_to_cstr(types);
+    return 0;
+}
+
+uint8_t get_resources(char **result, char* type_name) {
     std::vector<std::string> data_dirs;
     auto ral = lib::ral::create(data_dirs);
     auto opt_type = ral->find_type(std::string(type_name));
 
     if (!opt_type)
-        return NULL;
-
-    auto results = (*opt_type)->instances();
-    lib::json_emitter em {};
-    auto resources = em.parse_list(**opt_type, results);
-    return str_to_cstr(resources);
-}
-
-// TODO(ale): make the pointer approach work
-uint8_t get_all_with_err(char **resource,
-                         char *type_name) {
-    char *tmp = get_all(type_name);
-
-    if (resource == nullptr)
         return 1;
 
-    resource = &tmp;
+    auto resource_instances = (*opt_type)->instances();
+    lib::json_emitter em {};
+    auto resources = em.parse_list(**opt_type, resource_instances);
+    *result = str_to_cstr(resources);
     return 0;
 }
 
-struct outcome get_all_outcome(char* type_name_c) {
-    char *tmp = get_all(type_name_c);
-    uint8_t e_c = (tmp != NULL) ? 0 : 1;
-    struct outcome out = {tmp, e_c};
-    return out;
-}
 
-uint8_t get_types(char *types) {
+uint8_t get_resource(char **result, char* type_name, char* resource_name) {
+    std::vector<std::string> data_dirs;
+    auto ral = lib::ral::create(data_dirs);
+    auto opt_type = ral->find_type(std::string(type_name));
+
+    if (!opt_type)
+        return 1;
+
+    auto inst = (*opt_type)->find(std::string(resource_name));
+    lib::json_emitter em {};
+
+    auto resource = em.parse_find(**opt_type, inst);
+    *result = str_to_cstr(resource);
     return 0;
 }
